608388c91346a418f74ca2150554ce5e30a33841
tarajaneseton <tarajaneseton@gmail.com>
2022-11-24 19:05:16 +0000

    Update 3 files

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>My First React App</h1>
+        <h2>by Tara Jane Seton</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
