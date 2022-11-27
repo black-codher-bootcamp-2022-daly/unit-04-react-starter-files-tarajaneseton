bfd2b16889ceb749467e3a1657fcf56528748f03
tarajaneseton <tarajaneseton@gmail.com>
2022-11-27 18:17:59 +0000

    Update 3 files

--- first-react-app/tazapp/src/App.js
+++ first-react-app/tazapp/src/App.js
@@ -26,26 +26,11 @@ function App() {
     <div className="App">
       <Header className="header-tara" "Tara"  colour="blue" fruit="blueberry"/>
       <Header name="Tanya"  colour="orange" fruit="orange"/>
+      <Footer nickname="Jimy">
+        </div>
+        );
+        }
+        
 
-      <header className="App-header">
-        <h1>My First React App</h1>
-        <h2>by Tara Jane Seton</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer nickname="Taz"/>
-    </div>
-  );
-}
 
 export default App;
