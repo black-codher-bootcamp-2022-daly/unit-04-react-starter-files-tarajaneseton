89a5f6bd6c38a6146c0d217fb744e86b700c8817
tarajaneseton <tarajaneseton@gmail.com>
2022-11-27 18:21:05 +0000

    Update App.js

--- first-react-app/tazapp/src/App.js
+++ first-react-app/tazapp/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-// A react component can only return 1 element. in the example below, it will only return 1 
-// h1 element. To return 2 or more, it needs to be placed within a div. 
+import logo from "./logo.svg";
+import "./App.css";
 
 function Header(props) {
-  const { name, colour, fruit, className } = props;
+  const { name, color, fruit, className } = props;
   return (
-  <h1 className={className}>
-    {name} is {colour} like a {fruit}
+    <h1 className={className}>
+      {name} is {color} like a {fruit}
     </h1>
   );
 }
 
-function Footer({nickname}) {
+function Footer({ nickname }) {
   return (
-  <div>
-   <h1> goodbye {nickname}</h1>
+    <div>
+      <h1>goodbye {nickname}</h1>
     </div>
   );
 }
@@ -24,13 +21,15 @@ function Footer({nickname}) {
 function App() {
   return (
     <div className="App">
-      <Header className="header-tara" "Tara"  colour="blue" fruit="blueberry"/>
-      <Header name="Tanya"  colour="orange" fruit="orange"/>
-      <Footer nickname="Jimy">
-        </div>
-        );
-        }
-        
-
-
+      <Header
+        className="header-tobia"
+        name="Tobia"
+        color="blue"
+        fruit="blueberry"
+      />
+      <Header name="Tanya" color="orange" fruit="orange" />
+      <Footer nickname="Jimy" />
+    </div>
+  );
+}
 export default App;
