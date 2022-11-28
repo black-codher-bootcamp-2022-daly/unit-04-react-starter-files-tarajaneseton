45279e5cfe933a0efe1527d0181d5e4a973c2b8b
tarajaneseton <tarajaneseton@gmail.com>
2022-11-28 07:24:25 +0000

    2 functions and react test

---
+++ first-react-app/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { App, Header } from './App';
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/tobia/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders the header', () => {
+  render(<Header name="tobia" colour="pink" fruit="mango"/>);
+  const name = screen.getByText(/tobia/i);
+    const colour = screen.getByText(/pink/i);
+  const fruit = screen.getByText(/lychee/i);
+  expect(name).toBeInTheDocument();
+});
