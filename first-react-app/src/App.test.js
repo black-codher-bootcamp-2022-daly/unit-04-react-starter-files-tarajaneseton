e06108a7331e030c08ded3af1bde86ca35baf2b8
tarajaneseton <tarajaneseton@gmail.com>
2022-11-28 06:35:59 +0000

    Update App.test.js

--- first-react-app/src/App.test.js
+++ first-react-app/src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { App, Header } from './App';
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/taz/i);
+  expect(linkElement).toBeInTheDocument();
+});
 test('renders the header', () => {
   render(<Header />);
   const linkElement = screen.getByText(/taz/i);
