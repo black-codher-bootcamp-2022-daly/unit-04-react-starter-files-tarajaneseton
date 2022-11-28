edd5c794e4c05ba287bf7f4ea03e430ed417e9bb
tarajaneseton <tarajaneseton@gmail.com>
2022-11-28 06:47:03 +0000

    Update App.test.js

--- first-react-app/src/App.test.js
+++ first-react-app/src/App.test.js
@@ -7,7 +7,12 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 test('renders the header', () => {
-  render(<Header name="tobia" />);
-  const linkElement = screen.getByText(/tobia/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Header name="tobia" colour="pink" fruit="mango"/>);
+  const name = screen.getByText(/tobia/i);
+    const colour = screen.getByText(/pink/i);
+  const fruit = screen.getByText(/lychee/i);
+  expect(name).toBeInTheDocument();
+
+
+
 });
