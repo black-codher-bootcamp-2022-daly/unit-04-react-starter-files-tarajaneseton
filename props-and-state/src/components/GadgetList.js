1886334a776c9cbc00d722eb775e02a8fce4355f
tarajaneseton <tarajaneseton@gmail.com>
2022-11-30 21:28:51 +0000

    updated gadget list to have Gadget component that returns the title of each headphone

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
+function Gadget({item}) {
+
+  return <div>{item.title}</div>;
+}
+
 const GadgetList = (props) => {
 
   return (
     <main>
-      <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+      <h2>Headphones color {props.color}</h2>
+      {props.items.map((item) => (
+      <Gadget key={item.title} item={item} />
+      ))}
     </main>
   );
 }
 
 export default GadgetList;
 
-//items comes from the headphones list as an attribute on line 14 in index.js, It's an array of information on line 9.
+// component called gadgetlist, passing props to it.
+//items comes from the headphones list as an attribute on line 14 in index.js, It's an array of information on line 9 in index.js
 // map 
+
