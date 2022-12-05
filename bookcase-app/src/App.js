7d8ded2d8b080789b47fb5d27c04a7f2ed2b498d
tarajaneseton <tarajaneseton@gmail.com>
2022-12-05 21:04:23 +0000

    updated usestate, add background colour, margin and padding

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './components/Book';
 import data from './models/books.json';
 
+//useState 
 function App() {
-  const books = data;
-  return <ul>
-{books.map((book)=><Book book={book}/>)}
-    
-    </ul>
+  const [books] = useState(data);
+  return <div>
+    <h1> Tara's library</h1>
+{books.map((book) => <Book key={book.id} book={book} />)}
+</div> 
 }
 
 export default App;
-
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Book(props) {
-  const { title } = props.volumeInfo;
-  const { retailPrice } = props.saleInfo;
-  console.log(props.saleInfo)
+function Book({book}) {
+  const { title } = book.volumeInfo;
+  const { retailPrice } = book.saleInfo;
   return (
-    <div>
+    <div 
+    style={{
+      backgroundColor: "#d9ccff",
+      padding: "10px"
+      marginBottom: "4px"
+    }}>
       <h3>{title}</h3>
-      {retailPrice && retailPrice.amount < 2 ? "Great deal" : "Bestseller"}
+      {retailPrice && retailPrice.amount < 3 ? "Great deal" : "Bestseller"}
     </div>
     // above is a condition inside the react component. if retailPrice exists, AND if retail price property, amount is higher than 2 then print retailprice.amount, and if not it is free.
   );
+}
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    volumeInfo: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.array.isRequired,
+    description: PropTypes.string.isRequired,
+  }),
+  saleInfo: PropTypes.shape({
+    retailPrice: PropTypes.shape({
+      amount: PropTypes.number.isRequired,
+    }),
+    }),
+  }),
 }
 
 export default Book;
