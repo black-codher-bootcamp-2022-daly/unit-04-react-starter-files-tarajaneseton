0ca77e23e0125855867362f62d7840b9ca7e17cb
tarajaneseton <tarajaneseton@gmail.com>
2022-12-05 21:17:05 +0000

    added Authors

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -3,15 +3,22 @@ import PropTypes from "prop-types";
 
 function Book({book}) {
   const { title } = book.volumeInfo;
+  const { authors } = book.volumeInfo;
   const { retailPrice } = book.saleInfo;
   return (
     <div 
     style={{
       backgroundColor: "#d9ccff",
-      padding: "10px"
-      marginBottom: "4px"
+      padding: "10px",
+      marginBottom: "4px",
     }}>
       <h3>{title}</h3>
+      <h4>
+        {authors ?
+      authors.join(', '):
+      "No Authors Listed"
+      }
+      </h4>
       {retailPrice && retailPrice.amount < 3 ? "Great deal" : "Bestseller"}
     </div>
     // above is a condition inside the react component. if retailPrice exists, AND if retail price property, amount is higher than 2 then print retailprice.amount, and if not it is free.
