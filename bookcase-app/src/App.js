9371de2b0db892275a00f923bf0e75d71159e0ad
tarajaneseton <tarajaneseton@gmail.com>
2022-12-05 20:30:27 +0000

    bookcase app with map function and conditional rendering for book properties

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -4,7 +4,10 @@ import data from './models/books.json';
 
 function App() {
   const books = data;
-  return <Book/>;
+  return <ul>
+{books.map((book)=><Book book={book}/>)}
+    
+    </ul>
 }
 
 export default App;
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Book() {
-  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
+function Book(props) {
+  const { title } = props.volumeInfo;
+  const { retailPrice } = props.saleInfo;
+  console.log(props.saleInfo)
+  return (
+    <div>
+      <h3>{title}</h3>
+      {retailPrice && retailPrice.amount < 2 ? "Great deal" : "Bestseller"}
+    </div>
+    // above is a condition inside the react component. if retailPrice exists, AND if retail price property, amount is higher than 2 then print retailprice.amount, and if not it is free.
+  );
 }
 
 export default Book;
