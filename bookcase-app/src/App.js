2b6c7e6b7d3a62ec20ececdd3849dca9f5bb3476
tarajaneseton <tarajaneseton@gmail.com>
2022-12-07 18:41:38 +0000

    replaced code and add search.js

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
-import Book from './components/Book';
-import data from './models/books.json';
+import React, { useState } from "react";
+import Book from "./components/Book";
+import data from "./models/books.json";
 
-//useState 
+//useState
 function App() {
   const [books] = useState(data);
-  return <div>
-    <h1> Tara's library</h1>
-{books.map((book) => <Book key={book.id} book={book} />)}
-</div> 
+
+  return (
+  <Booklist>
+    {books.map((book) => {
+        <Book key={book.id} book={book} />
+    ))}
+  </Booklist);
+}
+
+function Booklist(props) {
+  return (
+    <div>
+      <h1>Tara's library</h1>
+    <div className="booklist-container">{props.children}</div>
+    </div>
+  );
 }
 
 export default App;
